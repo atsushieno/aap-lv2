f4744e306f05486af9d0bc5a916e7947848cdd95
atsushieno <atsushieno@gmail.com>
2020-06-16 17:17:00 +0900

    renamed aaplv2plugins to mda_lv2_plugins.

--- import-lv2-deps.sh
+++ import-lv2-deps.sh
@@ -4,7 +4,7 @@ ABIS_SIMPLE=(x86 x86_64 armeabi-v7a arm64-v8a)
 
 # Remove existing jniLibs for sanity
 rm -rf androidaudioplugin-lv2/src/main/jniLibs/
-rm -rf aaplv2plugins/src/main/jniLibs/
+rm -rf mda_lv2_plugins/src/main/jniLibs/
 
 
 for abi in ${ABIS_SIMPLE[*]}
@@ -14,21 +14,21 @@ do
     mkdir -p androidaudioplugin-lv2/src/main/jniLibs/$abi
     cp -R dependencies/dist/$abi/lib/*.so androidaudioplugin-lv2/src/main/jniLibs/$abi/
     # And then copy native libs of LV2 plugins for each ABI.
-    mkdir -p aaplv2plugins/src/main/jniLibs/$abi
-    cp -R dependencies/dist/$abi/lib/lv2/*/*.so aaplv2plugins/src/main/jniLibs/$abi/
+    mkdir -p mda_lv2_plugins/src/main/jniLibs/$abi
+    cp -R dependencies/dist/$abi/lib/lv2/*/*.so mda_lv2_plugins/src/main/jniLibs/$abi/
 done
 
 # Copy LV2 metadata files etc.
 # The non-native parts should be the same so we just copy files from x86 build.
-mkdir -p aaplv2plugins/src/main/assets/
-cp -R dependencies/dist/x86/lib/lv2/ aaplv2plugins/src/main/assets/
+mkdir -p mda_lv2_plugins/src/main/assets/
+cp -R dependencies/dist/x86/lib/lv2/ mda_lv2_plugins/src/main/assets/
 # ... except for *.so files. They are stored under jniLibs.
-rm aaplv2plugins/src/main/assets/lv2/*/*.so
+rm mda_lv2_plugins/src/main/assets/lv2/*/*.so
 
 # Generate `aap-metadata.xml` that AAP service look up plugins.
-mkdir -p aaplv2plugins/src/main/assets/lv2
-mkdir -p aaplv2plugins/src/main/res/xml
+mkdir -p mda_lv2_plugins/src/main/assets/lv2
+mkdir -p mda_lv2_plugins/src/main/res/xml
 tools/aap-import-lv2-metadata/build/aap-import-lv2-metadata \
-	aaplv2plugins/src/main/assets/lv2 \
-	aaplv2plugins/src/main/res/xml
+	mda_lv2_plugins/src/main/assets/lv2 \
+	mda_lv2_plugins/src/main/res/xml
 
