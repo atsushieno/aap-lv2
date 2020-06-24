1b2955121dbccf6f565d19e82bb7803c7946271f
Atsushi Eno <atsushieno@gmail.com>
2020-06-25 04:07:30 +0900

    simpified aap-mda-lv2 build.
    Remove asset/xml-only module, as the app part is also quite simple, almost
    just a template.

--- import-lv2-deps.sh
+++ import-lv2-deps.sh
@@ -4,7 +4,7 @@ ABIS_SIMPLE=(x86 x86_64 armeabi-v7a arm64-v8a)
 
 # Remove existing jniLibs for sanity
 rm -rf androidaudioplugin-lv2/src/main/jniLibs/
-rm -rf mda_lv2_plugins/src/main/jniLibs/
+rm -rf aap-mda-lv2/src/main/jniLibs/
 
 
 for abi in ${ABIS_SIMPLE[*]}
@@ -14,8 +14,8 @@ do
     mkdir -p androidaudioplugin-lv2/src/main/jniLibs/$abi
     cp -R dependencies/dist/$abi/lib/*.so androidaudioplugin-lv2/src/main/jniLibs/$abi/
     # And then copy native libs of LV2 plugins for each ABI.
-    mkdir -p mda_lv2_plugins/src/main/jniLibs/$abi
-    cp -R dependencies/dist/$abi/lib/lv2/*/*.so mda_lv2_plugins/src/main/jniLibs/$abi/
+    mkdir -p aap-mda-lv2/src/main/jniLibs/$abi
+    cp -R dependencies/dist/$abi/lib/lv2/*/*.so aap-mda-lv2/src/main/jniLibs/$abi/
 done
 
 # Copy LV2 metadata files etc.
@@ -25,15 +25,15 @@ done
 # But not that when you are copying this script, you should probably
 # NOT name a specific plugin.
 #
-mkdir -p mda_lv2_plugins/src/main/assets/lv2
-cp -R dependencies/dist/x86/lib/lv2/mda.lv2 mda_lv2_plugins/src/main/assets/lv2/
+mkdir -p aap-mda-lv2/src/main/assets/lv2
+cp -R dependencies/dist/x86/lib/lv2/mda.lv2 aap-mda-lv2/src/main/assets/lv2/
 # ... except for *.so files. They are stored under jniLibs.
-rm mda_lv2_plugins/src/main/assets/lv2/*/*.so
+rm aap-mda-lv2/src/main/assets/lv2/*/*.so
 
 # Generate `aap-metadata.xml` that AAP service look up plugins.
-mkdir -p mda_lv2_plugins/src/main/assets/lv2
-mkdir -p mda_lv2_plugins/src/main/res/xml
+mkdir -p aap-mda-lv2/src/main/assets/lv2
+mkdir -p aap-mda-lv2/src/main/res/xml
 tools/aap-import-lv2-metadata/build/aap-import-lv2-metadata \
-	mda_lv2_plugins/src/main/assets/lv2 \
-	mda_lv2_plugins/src/main/res/xml
+	aap-mda-lv2/src/main/assets/lv2 \
+	aap-mda-lv2/src/main/res/xml
 
