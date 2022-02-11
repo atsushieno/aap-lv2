8d8b326fd0791989c83f8dc6b63dbd9f8d4e0f82
atsushieno <atsushieno@gmail.com>
2022-02-11 09:19:21 +0900

    bump aap (0.6.23), AGP (D-alpha02), and NDK(r21d). release 0.1.22.
    Also imported existing midi tests from aap core to aap-ayumi.

--- aap-ayumi/src/androidTest/java/org/androidaudioplugin/aap_ayumi/MidiDeviceServiceTest.kt
+++ aap-ayumi/src/androidTest/java/org/androidaudioplugin/aap_ayumi/MidiDeviceServiceTest.kt
@@ -3,29 +3,28 @@ package org.androidaudioplugin.aap_ayumi
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.rule.ServiceTestRule
+import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginMidiDeviceServiceTesting
 import org.androidaudioplugin.androidaudioplugin.testing.AudioPluginServiceTesting
 import org.junit.Rule
 import org.junit.Test
 
-class PluginTest {
+class MidiDeviceServiceTest {
+
     private val applicationContext = ApplicationProvider.getApplicationContext<Context>()
-    private val testing = AudioPluginServiceTesting(applicationContext)
+    private val testing = AudioPluginMidiDeviceServiceTesting(applicationContext)
     @get:Rule
     val serviceRule = ServiceTestRule()
 
     @Test
-    fun getPluginServiceInfo() {
-        testing.getPluginServiceInfo()
-    }
-
-    @Test
-    fun basicServiceOperationsForAllPlugins() {
-        testing.basicServiceOperationsForAllPlugins()
+    fun basicServiceOperations() {
+        // Replace plugin name after copy-pasting this code.
+        testing.basicServiceOperations("aap-ayumi")
     }
 
     @Test
     fun repeatDirectServieOperations() {
         for (i in 0 until 5)
-            basicServiceOperationsForAllPlugins()
+            basicServiceOperations()
     }
+
 }
